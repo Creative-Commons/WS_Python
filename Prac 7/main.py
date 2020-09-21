ba16f21d5d91cc0e5a4b26ee819c1c79fd236125
Keane Pereira <keane3pereira@gmail.com>
2020-09-21 17:05:28 +0530

    prac 7: get, post

--- Prac 7/main.py
+++ Prac 7/main.py
@@ -1,20 +1,59 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, jsonify
+from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
+import json
 import models
 
-
 app = Flask(__name__)
+
+api = Api(app)
+CORS(app, support_credentials=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///prac7.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-@app.route("/")
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('price')
+
+class ProductList(Resource):
+    def get(self):
+        results = models.get_products()
+        products = []
+        for r in results:
+            products.append({
+                "id": r.id,
+                "name": r.name,
+                "price": r.price
+            })
+
+        return {"products": products}
+
+    def post(self):
+        data = request.get_json()
+        print(data)
+        name = str(data["name"])
+        price = float(data["price"])
+
+        res = models.create_product(name, price)
+        print(res)
+
+        return str(res)
+
+
+api.add_resource(ProductList, '/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+'''
 def home():
     products = models.get_products()
     return render_template("home.html", products = products)
 
-@app.route("/create_product", methods=["POST"])
+
 def create_product():
     name = request.form["name"]
     price = request.form["price"]
@@ -22,14 +61,12 @@ def create_product():
     print(res)
     return redirect("/")
 
-@app.route("/delete_product", methods=["POST"])
 def delete_product():
     _id = request.form["id"]
     res = models.delete_product(_id)
     print(res)
     return redirect("/")
 
-@app.route("/update_product", methods=["POST"])
 def update_product():
     _id = request.form["id"]
     name = request.form["name"]
@@ -37,6 +74,5 @@ def update_product():
     res = models.update_product(_id, name, price)
     print(res)
     return redirect("/")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    
+'''
