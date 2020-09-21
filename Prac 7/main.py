8e7b19f1fb45fb6bbbfb07883531d4a4921ed78b
Keane Pereira <keane3pereira@gmail.com>
2020-09-21 22:22:41 +0530

    prac7-done

--- Prac 7/main.py
+++ Prac 7/main.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, render_template, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import json
 import models
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///prac7.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+
 
 @app.route('/', methods=['GET'])
 def query_records():
@@ -20,64 +23,39 @@ def query_records():
             "name": r.name,
             "price": r.price
         })
-
     return {"products": products}
+
 
 @app.route('/', methods=['POST'])
 def create_record():
     ''' CREATE '''
     data = request.get_json()
-    print(data)
     name = str(data["name"])
     price = float(data["price"])
-
     res = models.create_product(name, price)
-    print(res)
-
     return str(res)
+
 
 @app.route('/', methods=['PUT'])
 def update_record():
     ''' UPDATE '''
     data = request.get_json()
-    print(data)
-    return
+    _id = int(data["id"])
+    name = data["name"]
+    price = float(data["price"])
+    res = models.update_product(_id, name, price)
+    return jsonify(res)
+
 
 @app.route('/', methods=['DELETE'])
 def delete_record():
     ''' DELETE '''
-    print(data)
-    return
+    data = request.get_json()
+    _id = int(data["id"])
+    #print(data)
+    res = models.delete_product(_id)
+    return jsonify(res)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-'''
-def home():
-    products = models.get_products()
-    return render_template("home.html", products = products)
-
-
-def create_product():
-    name = request.form["name"]
-    price = request.form["price"]
-    res = models.create_product(name, price)
-    print(res)
-    return redirect("/")
-
-def delete_product():
-    _id = request.form["id"]
-    res = models.delete_product(_id)
-    print(res)
-    return redirect("/")
-
-def update_product():
-    _id = request.form["id"]
-    name = request.form["name"]
-    price = request.form["price"]
-    res = models.update_product(_id, name, price)
-    print(res)
-    return redirect("/")
-    
-'''
